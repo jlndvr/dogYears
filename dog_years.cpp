9c101343b0f2c574904ea4900318f49c2c4b8f46
Josué Obed Landaverde <129899297+jlndvr@users.noreply.github.com>
2025-05-08 14:33:44 -0500

    Create dog_years.cpp

---
+++ dog_years.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  // My dog is 2
+  int dog_age = 3;
+  int early_years, later_years, human_years;
+  // Set to 21
+  early_years = 21;
+  // Equation
+  later_years = (dog_age - 2) * 4;
+  human_years = early_years + later_years;
+  std::cout<< "My name is Smokey! Ruff ruff, I am " << human_years << " years old in human years.\n";
+}
